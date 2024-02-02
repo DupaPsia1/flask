d656fd0c56a2ff2b2425635f1eb55d9abba576fa
DupaPsia1 <129112889+s22408@users.noreply.github.com>
2024-02-02 21:37:51 +0100

    update

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template;
+from flask import Flask, render_template, request;
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+
+        username = request.form.get('login')
+        password = request.form.get('password')
+        
+        print("POST REQUEST WITH: "+str(username)+" "+str(password))
+        return f"Successfully registered user {username} with password: {password}"
+    
+    # If it's a GET request, render the registration form
+    return "TO JE GET REQUEST WE NO"
